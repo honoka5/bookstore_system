12dc19a9f5b6120302b805fcb0b50f282f686136
Yuki-perform <kaiyakutarou@gmail.com>
2025-05-22 09:18:35 +0900

    ブランチ共有のテスト

--- config/app_local.example.php
+++ config/app_local.example.php
@@ -44,10 +44,10 @@ return [
              */
             //'port' => 'non_standard_port_number',
 
-            'username' => 'my_app',
+            'username' => 'root',
             'password' => 'secret',
 
-            'database' => 'my_app',
+            'database' => 'cakephp',
             /*
              * If not using the default 'public' schema with the PostgreSQL driver
              * set it here.
