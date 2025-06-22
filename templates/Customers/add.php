a2168df30f5e0845cec5a84b3f3cca76fcb98a48
honoka5 <s2370179@s.do-johodai.ac.jp>
2025-06-22 19:53:19 +0900

    Excelをアップロードするだけでよいので、自分で書く処理は不要。

--- templates/Customers/add.php
+++ templates/Customers/add.php
@@ -5,14 +5,6 @@
  */
 ?>
 <h1>顧客登録</h1>
-<?= $this->Form->create($customer) ?>
-    <?= $this->Form->control('name', ['label' => '顧客名']) ?>
-    <?= $this->Form->control('phone_number', ['label' => '電話番号']) ?>
-    <?= $this->Form->control('contact_person', ['label' => '担当者名']) ?>
-    <?= $this->Form->button('登録') ?>
-<?= $this->Form->end() ?>
-<hr>
-
 <h2>Excelで一括登録</h2>
 <?= $this->Form->create(null, ['type' => 'file']) ?>
     <?= $this->Form->control('excel_file', ['type' => 'file', 'label' => 'Excelファイル']) ?>
