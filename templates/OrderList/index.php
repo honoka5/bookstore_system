1d0302feb940c9852d8997e289a209ddf36e902a
awa012 <reo18531853@gmail.com>
2025-07-23 14:51:00 +0900

    注文書リストにパンくずを追加した

--- templates/OrderList/index.php
+++ templates/OrderList/index.php
@@ -179,7 +179,7 @@
     </style>
 </head>
 <body>
-    <?= $this->element('header', ['title' => '注文書一覧']) ?>
+    <?= $this->element('common_header') ?>
     <div class="main-container" style="border:2px solid #222;">
         <div class="content-area">
             <!-- New Create Button -->
