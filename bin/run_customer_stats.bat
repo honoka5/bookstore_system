a6dd7d9c2348bc393e8221a102b869c7b62adcb9
Yuki <kaiyakutarou@gmail.com>
2025-07-27 15:22:34 +0900

    Update run_customer_stats.bat

--- bin/run_customer_stats.bat
+++ bin/run_customer_stats.bat
@@ -3,7 +3,7 @@ REM CakePHP CustomerStatsCommand バッチ実行用
 cd /d %~dp0..
 REM PHPのパスを環境に合わせて修正してください
 set PHP_PATH=C:\xampp\php\php.exe
-set APP_PATH=C:\xampp\htdocs\php\sisukai\bin\cake.php
+set APP_PATH=C:\xampp\htdocs\php\bookstore_system\bin\cake.php
 
 "%PHP_PATH%" "%APP_PATH%" customer_stats > "C:\xampp\htdocs\php\sisukai\logs\customer_stats.log" 2>&1
 exit /b %ERRORLEVEL%
